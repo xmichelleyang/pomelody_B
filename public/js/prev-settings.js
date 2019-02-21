7119c42eb23e78ed55c2aa69addcd4a5108557da
Michelle Yang <zomgitsmichelle@gmail.com>
2019-02-20 16:44:06 -0800

    Made cancels just return.

--- public/js/prev-settings.js
+++ public/js/prev-settings.js
@@ -1,11 +1,19 @@
 
 function editProdPlaylist(){
 	var change = prompt('What would you like to change your productivity playlist to?');
+	// If user presses cancel
+	if (change == null) {
+    	return
+    }
 	verifyURL(change, "prod");
 }
 
 function editProdTime(){
 	var change = prompt('What would you like to change your productivity time interval to?');
+	// If user presses cancel
+	if (change == null) {
+    	return
+    }
 	// Verify its just a number 
 	while (isNaN(change)) {
         alert("Please enter a valid numerical value");
@@ -18,18 +26,28 @@ function editProdTime(){
 
 function editRelaxPlaylist(){
 	var change = prompt('What would you like to change your relaxation playlist to?');
+	// If user presses cancel
+	if (change == null) {
+    	return
+    }
 	verifyURL(change, "relax");
 
 }
 
 function editRelaxTime(){
 	var change = prompt('What would you like to change your relaxation playlist to?');
+	// If user presses cancel
+    if (change == null) {
+    	return
+    }
+
 	// Verify its just a number 
 	while (isNaN(change)) {
         alert("Please enter a valid numerical value");
         change = prompt("Please enter your desired time");
     }
     // Update value on JSON and page
+    console.log("changed value to ", change);
 	updateRelaxTimeVal(change + " min");
 	$('#relaxTimeInfo').text(change + " min");
 }
