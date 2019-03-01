b4ed8b6325fc049d391d8fadfb77d5c348b88afb
Saveree Joshipura <sjoshipu@ucsd.edu>
2019-02-28 19:45:34 -0800

    B

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ if ('development' == app.get('env')) {
 app.get('/', render.viewLogin);
 app.get('/login', render.viewLogin)
 app.get('/welcome', render.viewWelcome);
+app.get('/instructions', render.viewInstructions);
 app.get('/help-screen',render.viewHelpScreen);
 app.get('/prev-settings', render.viewPrevSettings);
 app.get('/select-productivity', render.viewSelectProd);
