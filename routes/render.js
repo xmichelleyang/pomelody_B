6ee51b8c3e20444b61768afbc1c35dc4798caa8e
Michelle Yang <zomgitsmichelle@gmail.com>
2019-03-03 21:53:17 -0800

    getSpotifyToken now uses await because fs has been promisified with bluebird.

--- routes/render.js
+++ routes/render.js
@@ -2,10 +2,10 @@
 const data = require('../data.json');
 const $ = require('jquery');
 const param = require('jquery-param');
-const fs = require('fs');
 const axios = require('axios');
 const spotify = require('../api_handler/spotify')
-
+const Promise = require('bluebird')
+const fs = Promise.promisifyAll(require('fs'));
 
 exports.viewLogin = function(request, response){
 	console.log("hi im working uwu");
@@ -76,65 +76,60 @@ exports.viewEnd = function(request, response){
 	response.render('end-page');
 };
 
-exports.getSpotifyToken = function(request, response) {
+exports.getSpotifyToken = async function(request, response) {
 	console.log("Correctly inside viewSpotify call");
 	var client_id = "b4c291c0effb4b64bd6b961dc52fab74"; // Your client id
-	var client_secret = "";
-	fs.readFile('not-secret-key.txt', (err, data) => {
-							  if (err) throw err;
-							  console.log(data);
-							  client_secret = data
-								
-								// The code we get back from Spotify
-								var code = request.query.code;
-								// Body object to be parametized for details
-								var body = {
-									grant_type: "authorization_code",
-									code: code,
-									redirect_uri: "http://localhost:3000/spotifyLogin"
-								}
-								paramed_body = param(body);
-								// The configuration for the post request
-								var details = 	{
-									url: 'https://accounts.spotify.com/api/token',
-									method: 'POST',
-									data: paramed_body,
-									headers: {
-										'Content-Type' : 'application/x-www-form-urlencoded',
-										'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-									}
-								}
-								// Get the tokens
-								var access_token, refresh_token;
-								axios.request(details).then(function(res) {
-									access_token = res.data.access_token
-							    refresh_token = res.data.refresh_token
+	var client_secret = await fs.readFileAsync('not-secret-key.txt');
+	console.log("client secret is", client_secret);
 
-									// Send tokens into spotify js handler
-									spotify.setAccessInfo(access_token, refresh_token)
+	// The code we get back from Spotify
+	var code = request.query.code;
+	// Body object to be parametized for details
+	var body = {
+		grant_type: "authorization_code",
+		code: code,
+		redirect_uri: "http://localhost:3000/spotifyLogin"
+	}
+	paramed_body = param(body);
+	// The configuration for the post request
+	var details = 	{
+		url: 'https://accounts.spotify.com/api/token',
+		method: 'POST',
+		data: paramed_body,
+		headers: {
+			'Content-Type' : 'application/x-www-form-urlencoded',
+			'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+		}
+	}
+	// Get the tokens
+	var access_token, refresh_token;
+	axios.request(details).then(function(res) {
+		access_token = res.data.access_token
+    refresh_token = res.data.refresh_token
 
-									response.redirect('welcome')
-								}).catch( function (error) {
-									if (error.response) {
-										// The request was made and the server responded with a status code
-										// that falls out of the range of 2xx
-										console.log("data", error.response.data);
-										console.log("status", error.response.status);
-										console.log("headers", error.response.headers);
-									} else if (error.request) {
-										// The request was made but no response was received
-										// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-										// http.ClientRequest in node.js
-										console.log(error.request);
-									} else {
-										// Something happened in setting up the request that triggered an Error
-										console.log('Error', error.message);
-									}
-									console.log(error.config);
-								});
+		// Send tokens into spotify js handler
+		spotify.setAccessInfo(access_token, refresh_token)
 
+		response.redirect('welcome')
+	}).catch( function (error) {
+		if (error.response) {
+			// The request was made and the server responded with a status code
+			// that falls out of the range of 2xx
+			console.log("data", error.response.data);
+			console.log("status", error.response.status);
+			console.log("headers", error.response.headers);
+		} else if (error.request) {
+			// The request was made but no response was received
+			// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+			// http.ClientRequest in node.js
+			console.log(error.request);
+		} else {
+			// Something happened in setting up the request that triggered an Error
+			console.log('Error', error.message);
+		}
+		console.log(error.config);
+	});
 
-						});
 
 
 }
