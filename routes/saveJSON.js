e77533a2e8f61aa35717f2831938ff7e3efb3e78
Michelle Yang <zomgitsmichelle@gmail.com>
2019-03-05 15:44:50 -0800

    Wondering if relative path is what is breaking heroku.com

--- routes/saveJSON.js
+++ routes/saveJSON.js
@@ -1,9 +1,10 @@
 exports.saveJSON = function(req, res) {
 	var fs = require('fs');
 	console.log(req.body);
-	fs.writeFile("./data.json", JSON.stringify(req.body, null, 4), (err) => {
+	fs.writeFile("../data.json", JSON.stringify(req.body, null, 4), (err) => {
 	  if (err) {
 	      console.error(err);
+				res.send(400);
 	      return;
 	  };
 	  console.log("File has been created");
