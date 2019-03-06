a8ce84853c9034274ad64d2890975773e9c04e77
Michelle Yang <zomgitsmichelle@gmail.com>
2019-03-05 16:00:47 -0800

    Require inside the data info to test to see if it dynamically reads it

--- routes/data.js
+++ routes/data.js
@@ -1,9 +1,9 @@
 // This is just an example of what has to be donew
 
 
-var data = require('../data.json');
 
 exports.dataInfo = function(req, res) {
+	var data = require('../data.json');
 	console.log("Trying to read in", __dirname);
 	console.log("I see ", data);
 	res.json(data);
